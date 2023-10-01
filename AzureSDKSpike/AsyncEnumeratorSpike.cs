d0e8699d834fbeee12d4e2480e6357f38ce7564e
Tsuyoshi Ushio <ushio@simplearchitect.com>
2023-10-01 10:39:02 -0700

    Add project files.

---
+++ AzureSDKSpike/AsyncEnumeratorSpike.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+
+namespace AzureSDKSpike
+{
+    public class AsyncEnumeratorSpike
+    {
+        public async ValueTask ExecuteAsync()
+        {
+            await foreach (var number in RangeAsync(0, 5))
+            {
+                Console.WriteLine(number);
+            }
+
+            CancellationTokenSource cts = new CancellationTokenSource(400);
+            await foreach(var number in RangeWithCancellationAsync(0, 10).WithCancellation(cts.Token))
+            {
+                // Expect that it will finish at 4.
+                Console.WriteLine(number);
+            }
+
+            // Reactive support IAsyncEnumerator
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1))
+                .Take(5)
+                .Select(l => (int)l);
+            await foreach(var number in observable.ToAsyncEnumerable())
+            {
+                Console.WriteLine($"from Rx: {number}");
+            }
+
+        }
+
+        private async IAsyncEnumerable<int> RangeAsync(int start, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                await Task.Delay(100);
+                yield return start + i;
+            }
+        }
+
+        private async IAsyncEnumerable<int> RangeWithCancellationAsync(int start, int count, [EnumeratorCancellation] CancellationToken token = default)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                try
+                {
+                    await Task.Delay(100, token);
+                }
+                catch (TaskCanceledException)
+                {
+                    Console.WriteLine("Task was cancelled.");
+                    break;  // Without this break, iterator keep on going.
+                }
+                yield return start + i;
+            }
+        }
+    }
+}
---
+++ AzureSDKSpike/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Azure;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using System.Linq;
+using Azure;
+using AzureSDKSpike;
+
+// local emulator  "https://127.0.0.1:10000/devstoreaccount1"
+
+
+var spike = new AsyncEnumeratorSpike();
+await spike.ExecuteAsync();
+
+var storageAccountUri = Environment.GetEnvironmentVariable("StorageAccountUri") ?? string.Empty;
+
+// Configuration Settings by DI.
+var services = new ServiceCollection();
+services.AddAzureClients(x =>
+{
+    x.AddBlobServiceClient(new Uri(storageAccountUri));
+    x.UseCredential(new DefaultAzureCredential());
+});
+
+// Configuration Settings by Client
+var blobServiceClient = new BlobServiceClient(
+    new Uri(storageAccountUri),
+    new DefaultAzureCredential());
+
+if (blobServiceClient == null)
+{
+    return;
+}
+
+var containers = blobServiceClient.GetBlobContainersAsync();
+await foreach(var container in containers)
+{
+    Console.WriteLine($"container: {container.Name} is already exists.");
+}
+
+string containerName = "test";
+
+var blobContainerClient = new BlobContainerClient(new Uri($"{storageAccountUri}/{containerName}"), new DefaultAzureCredential());
+await blobContainerClient.CreateIfNotExistsAsync();
+
+try
+{
+    // this method can throw an exception if the container already exists.
+    var result = await blobServiceClient.CreateBlobContainerAsync("test");
+} 
+catch (RequestFailedException ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
